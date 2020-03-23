3918bf7abfe2db24f29527464d24a248ee7d83da
Michael Malov <mmeemail@gmail.com>
2020-03-23 20:18:46 +0300

    randomly failing tests

---
+++ test.rb
@@ -0,0 +1,51 @@
+require "test/unit"
+
+class RandomFailingTest < Test::Unit::TestCase
+  def test_01
+    probably_fail
+  end
+
+  def test_02
+    probably_fail
+  end
+
+  def test_03
+    probably_fail
+  end
+
+  def test_04
+    probably_fail
+  end
+
+  def test_05
+    probably_fail
+  end
+
+  def test_06
+    probably_fail
+  end
+
+  def test_07
+    probably_fail
+  end
+
+  def test_08
+    probably_fail
+  end
+
+  def test_09
+    probably_fail
+  end
+
+  def test_10
+    probably_fail
+  end
+
+  def probably_fail
+    if rand(100) > 80
+      flunk 'Test failed'
+    else
+      assert true
+    end
+  end
+end
